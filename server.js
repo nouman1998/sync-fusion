6dd71d73ccd5c75989604bbea9dc8f537902508b
Nouman Ejaz <nouman.ejaz198@gmail.com>
2021-09-06 02:31:32 +0500

    updates

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files  form the dist directory
-app.use(express.static(__dirname + './dist/sync-fusion'));
-
-app.get('/*', function(req, res) {
-
-    res.sendFile(path.join(__dirname + './dist/sync-fusion/index.html'));
+const ngApp = express();
+ngApp.use(express.static('./dist/sync-fusion'));
+ngApp.get('/*', function(request, response) {
+    response.sendFile(path.join(__dirname, '/dist/sync-fusion/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+ngApp.listen(process.env.PORT || 8080);
